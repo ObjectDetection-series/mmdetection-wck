b96fb0754a59b3585cf96ba1ec8d5824b7ff4c03
WangCK0809 <2024685259@qq.com>
2019-12-24 19:22:16 +0800

    Date:12.24 - PC:WangCK

--- tools_kai/train_eval_test/train_detector_batch.py
+++ tools_kai/train_eval_test/train_detector_batch.py
@@ -27,13 +27,16 @@ def main():
             # '../../configs/cvc09/faster_rcnn_v16_c5_cvc.py',
             # '../../configs/cvc09/faster_rcnn_v16_fpn_cvc.py',
 
-            # dataset:kaist   backbone:r50
-            '../../configs/kaist/mul_faster_rcnn_r50_mlfpn_add_kaist.py',
-            '../../configs/kaist/mul_faster_rcnn_r50_mlfpn_cat_kaist.py',
-            '../../configs/kaist/mul_faster_rcnn_r50_pre_mlfpn_add_kaist.py',
-            '../../configs/kaist/mul_faster_rcnn_r50_pre_mlfpn_cat_kaist.py',
-            '../../configs/kaist/mul_faster_rcnn_r50_c4_add_kaist.py',
-            '../../configs/kaist/mul_faster_rcnn_r50_c4_cat_kaist.py',
+            # Author:YY     dataset:kaist       backbone:r50
+            '../../configs/kaist_fpn/mul_faster_rcnn_r50_fpn_add_kaist.py',
+
+            # Author:WangCK     dataset:kaist       backbone:r50
+            # '../../configs/kaist/mul_faster_rcnn_r50_mlfpn_add_kaist.py',
+            # '../../configs/kaist/mul_faster_rcnn_r50_mlfpn_cat_kaist.py',
+            # '../../configs/kaist/mul_faster_rcnn_r50_pre_mlfpn_add_kaist.py',
+            # '../../configs/kaist/mul_faster_rcnn_r50_pre_mlfpn_cat_kaist.py',
+            # '../../configs/kaist/mul_faster_rcnn_r50_c4_add_kaist.py',
+            # '../../configs/kaist/mul_faster_rcnn_r50_c4_cat_kaist.py',
 
 
             # dataset:kaist   backbone:v16
@@ -53,11 +56,11 @@ def main():
                 mmdet_version=__version__, config=cfg.text)
 
         username = getpass.getuser()
-        # temp_file = '/home/' + username + '/WangCK/Data/temp/temp.txt'
-        temp_file = '/media/' + username + '/3rd/WangCK/Data/temp/temp.txt'
+        temp_file = '/home/' + username + '/WangCK/Data/temp/temp.txt'
+        # temp_file = '/media/' + username + '/3rd/WangCK/Data/temp/temp.txt'
         fo = open(temp_file, 'w+')
-        str_write = cfg.work_dir.replace('../..', ('/media/'+username+'/3rd/WangCK/Data'))
-        # str_write = cfg.work_dir.replace('../..', ('/home/' + username + '/WangCK/Data'))
+        # str_write = cfg.work_dir.replace('../..', ('/media/'+username+'/3rd/WangCK/Data'))
+        str_write = cfg.work_dir.replace('../..', ('/home/' + username + '/WangCK/Data'))
         fo.write(str_write)
         fo.close()
 
